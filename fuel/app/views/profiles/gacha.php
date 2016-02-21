b52ddacfed741df9488000dabdde23c883b9f819
y-ohgi <ohgi.211@gmail.com>
2016-02-21 23:18:21 +0900

    fix answer.md

--- fuel/app/views/profiles/gacha.php
+++ fuel/app/views/profiles/gacha.php
@@ -11,12 +11,13 @@
 
     <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
     <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="js/jquery.leanModal.min.js"></script>
     <script>
 
      // とても隠したい関数
      function resultHtmlGen(imgpath, title, desc, url){
        imgpath?imgpath:imgpath = "images/gachaball.jpg";
-       return "<table><tr><td colspan=2><img src=" + imgpath +"></td></tr> <tr><th>タイトル: </th><td>"+title+"</td></tr> <tr><th>説明: </th><td>"+desc+"</td></tr> <tr><th>URL: </th><td>"+url+"</td></tr> </table>";
+       return "<table><tr><td colspan=2><img style='max-height:300px' src=" + imgpath +"></td></tr> <tr><th>タイトル: </th><td>"+title+"</td></tr> <tr><th>説明: </th><td>"+desc+"</td></tr> <tr><th>URL: </th><td>"+url+"</td></tr> </table>";
      }
      
      $(document).ready(function(){
@@ -25,31 +26,105 @@
        $("#gacha").click(function(){
 
 	 $("#result").fadeOut(function(){
-	 
-	 $.ajax({
-	   url: "api/gacha"
-	 }).done(function(data){
-	   $("#result").empty();
-	   console.log(data);
 	   
+	   $.ajax({
+	     url: "api/gacha"
+	   }).done(function(data){
+	     $("#result").empty();
+	     console.log(data);
+	     
 
-	   $("#result").append(resultHtmlGen(data[0]['imagepath'], data[0]['title'], data[0]['desc'], data[0]['url']));
-	   
-	   $("#result").show();
-	 }).fail(function(data){
-	   alert("error");
-	   console.log(data);
+	     $("#result").append(resultHtmlGen(data[0]['imagepath'], data[0]['title'], data[0]['desc'], data[0]['url']));
+	     
+	     $("#result").show();
+	   }).fail(function(data){
+	     alert("error");
+	     console.log(data);
 	   });
 
-	   });
+	 });
        });
        
      });
     </script>
 
     <style>
+
+     html, body{
+       height: 100%;
+     }
+
+     /***
+      * header
+     */
+     header {
+       text-align:center;
+       color: white;
+       font-weight: bold;
+       font-size: 1.2em;
+       
+       width: 100%;
+       height: 40px;
+
+       background: black;
+       border-bottom: 1px solit white;
+     }
+
+
+     /***
+      * main content
+      */
+     .container {
+       height: 100%;
+       
+       background: gray;
+     }
+     
+     .gacha_box{
+       position: absolute;
+       top: 20%;
+       left: 50%;
+       transform: translateX(-50%);
+     }
+
+     button#gacha {
+       position: absolute;
+       left: 50%;
+       top: 30%;
+       transform: translateX(-50%);
+       border: none;
+       padding: 20px 50px;
+       border-radius: 2px;
+       background: #E4FCFF;
+       box-shadow: gray 2px 2px 2px 2px;
+       font-size: 1.2em;
+       font-weight: bold;
+       color: currentColor;
+       text-shadow: white 1px 2px;
+     }
+
+     
+
+     /* モーダル計 */
      .result{
        display:none;
+     }
+
+     #lean_overlay{
+       position: fixed; z-index:100;
+       top: 0px;
+       left: 0px;
+       height: 100%;
+       width: 100%;
+       background: #000;
+       display: none;
+     }
+     #result{
+       background: none repeat scroll 0 0 #FFFFFF;
+       box-shadow: 0 0 4px rgba(0, 0, 0, 0.7);
+       display: none;
+       padding: 30px;
+       width: 780px;
      }
     </style>
 
@@ -57,10 +132,14 @@
   </head>
   
   <body>
-    <h1>ガチャ</h1>
+    <header>
+      <h1>ポートフォリオガチャ</h1>
+    </header>
 
-    <img alt="" src="images/gachagacha.png"/>
-    <button id="gacha">ガチャる</button>
+    <div class="container">
+      <img alt="" src="images/gachagacha.png" class="gacha_box"/>
+      <button rel="leanModal" href="#result" id="gacha">ガチャる</button>
+    </div>
 
     <div class="result" id="result">
       <table>
@@ -82,7 +161,16 @@
       </table>
     </div>
     
-    
+
+    <script type="text/javascript">
+     $(function() {
+       $( 'button[rel*=leanModal]').leanModal({
+	 top: 50,
+	 overlay : 0.5,
+	 closeButton: ".modal_close"
+       });
+     });
+    </script>
   </body>
 </html>
 
